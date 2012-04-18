929793c13c1918bb5007d2cbe8187f2934cdfc1f
Alex Beal <alexlbeal@gmail.com>
2012-04-18 22:46:35 +0400

    Prediction list sorts by most recently used.
    1) The CFG is now composed for PageStat structs that keep track of (a)
    the page it represents, (b) the frequency of when it gets inserved, and
    (c) the timestamp corresponding to that page.
    2) That information is used to sort an array of predictions (PageStat
    structs) by the timestamp, into most recently used order. The most
    recently used is the first to attempt to be swapped in.
    3) Added some comments.

--- pager-predict.c
+++ pager-predict.c
@@ -62,58 +62,128 @@ int calc_page(int pc){
 }
 
 #define EMPTY -1
+struct page_stat{
+    int page;
+    int freq;
+    int *timestamp;
+};
 
-void init_cfg(int cfg[MAXPROCESSES][MAXPROCPAGES][MAXPROCPAGES]){
+typedef struct page_stat PageStat;
+
+void init_cfg(PageStat cfg[MAXPROCESSES][MAXPROCPAGES][MAXPROCPAGES]){
     int i, j, k;
     for(i=0; i<MAXPROCESSES; i++){
     for(j=0; j<MAXPROCESSES; j++){
     for(k=0; k<MAXPROCESSES; k++){
-        cfg[i][j][k] = EMPTY;
+        cfg[i][j][k].page = EMPTY;
+        cfg[i][j][k].freq = -1;
+        cfg[i][j][k].timestamp = NULL;
     }
     }
     }
 }
 
-void insert_cfg(int cur_page, int proc, int last_page, int cfg[MAXPROCESSES][MAXPROCPAGES][MAXPROCPAGES]){
+/* Insert into the cfg, given the last_page, the cur_page and
+ * the proc. For example, if cur_page=1, last_page=0, and proc=9,
+ * then '1' will be inserted into the cfg for page zero, proc 9,
+ * signalling that sometime in the past, proc 9 went from page 0
+ * to page 1.
+ */
+void insert_cfg(
+        int cur_page,
+        int proc,
+        int last_page,
+        PageStat cfg[MAXPROCESSES][MAXPROCPAGES][MAXPROCPAGES],
+        int timestamps[MAXPROCESSES][MAXPROCPAGES]){
     int i;
-    int *slots;
+    PageStat *slots;
     slots = cfg[proc][last_page];
 
     for(i=0; i<MAXPROCPAGES; i++){
-        if(slots[i] == cur_page){
+        if(slots[i].page == cur_page){
+            slots[i].freq++;
             break;
         }
-        if(slots[i] == EMPTY){
-            slots[i] = cur_page;
+        if(slots[i].page == EMPTY){
+            slots[i].page = cur_page;
+            slots[i].freq = 1;
+            slots[i].timestamp = &(timestamps[proc][i]);
             break;
         }
     }
 }
 
-void print_cfg(int cfg[MAXPROCESSES][MAXPROCPAGES][MAXPROCPAGES]){
+/* Debug function for printing the control flow graph. */
+void print_cfg(PageStat cfg[MAXPROCESSES][MAXPROCPAGES][MAXPROCPAGES]){
     int i, j, k;
     for(i=0; i<MAXPROCESSES; i++){
         printf("Proc %d:\n", i);
-    for(j=0; j<MAXPROCESSES; j++){
-        printf("Page %d: ", j);
-    for(k=0; k<MAXPROCESSES; k++){
-        printf("%d, ", cfg[i][j][k]);
-    }
-        printf("\n");
-    }
+        for(j=0; j<MAXPROCESSES; j++){
+            printf("Page %d: ", j);
+            for(k=0; k<MAXPROCESSES; k++){
+                printf("%d,%d; ", cfg[i][j][k].page, cfg[i][j][k].freq);
+            }
+            printf("\n");
+        }
     }
 }
 
-int* pred_page(int proc, int cur_pc, int cfg[MAXPROCESSES][MAXPROCPAGES][MAXPROCPAGES]){
-    return cfg[proc][calc_page(cur_pc+100)];
+/* Predict pages
+ * Return an array of pages that are most likely to
+ * be needed in 100 ticks. The returned array is
+ * MAXPROCPAGES long and empty cells are -1
+ */
+PageStat* pred_page(int proc, int cur_pc, PageStat cfg[MAXPROCESSES][MAXPROCPAGES][MAXPROCPAGES]){
+    return cfg[proc][calc_page(cur_pc+101)];
 }
 
-void print_guess(int* guesses){
+/* Count how many non-empty PageStat elements are in an
+ * array of PageStat structs.
+ */
+int page_count(PageStat *guesses){
+    int len=0;
+    while(guesses[len].page != EMPTY && len<MAXPROCPAGES)
+        len++;
+    return len;
+}
+
+/* Swap two PageStat elements */
+void swap(PageStat* l, PageStat* r){
+    PageStat tmp=*l;
+    *l=*r;
+    *r=tmp;
+}
+
+/* guesses: array of PageStat.
+ * Desc: Sorts the array according to biggest timestamp
+ *  (most recently used).
+ */
+void page_sort(PageStat *guesses){
+    int len = page_count(guesses);
+    int i;
+    int swapped = 0;
+    do{
+        swapped=0;
+        for(i=1;i<len;i++){
+            //if(guesses[i-1].freq<guesses[i].freq){
+            assert(guesses[i-1].timestamp != NULL &&
+                   guesses[i].timestamp != NULL);
+            if(*(guesses[i-1].timestamp) < *(guesses[i].timestamp)){
+                swap(guesses+(i-1), guesses+i);
+                swapped = 1;
+            }
+        }
+    }while(swapped);
+}
+
+/* Desc: Prints out an array of PageStat for debugging. */
+void print_guess(PageStat* guesses){
     int i;
     for(i=0; i<MAXPROCPAGES; i++){
-        if(guesses[i] == EMPTY)
+        if(guesses[i].page == EMPTY)
             break;
-        printf("%d, ", guesses[i]);
+        assert(guesses[i].timestamp != NULL);
+        printf("%d,%d,%d; ", guesses[i].page, guesses[i].freq, *(guesses[i].timestamp));
     }
     printf("\n");
 }
@@ -132,7 +202,7 @@ void pageit(Pentry q[MAXPROCESSES]) {
 
     /* Each process has a 20 item long array, with each element
      * pointing to another 20 element array */
-    static int cfg[MAXPROCESSES][MAXPROCPAGES][MAXPROCPAGES];
+    static PageStat cfg[MAXPROCESSES][MAXPROCPAGES][MAXPROCPAGES];
 
     /* Local vars */
     int proctmp;
@@ -170,7 +240,7 @@ void pageit(Pentry q[MAXPROCESSES]) {
             continue;
         /* While we're at it, page out the last page */
         pageout(proctmp, last_page);
-        insert_cfg(cur_page, proctmp, last_page, cfg);
+        insert_cfg(cur_page, proctmp, last_page, cfg, timestamps);
     }
 
     /* Update all the timestamps for active procs */
@@ -222,16 +292,18 @@ void pageit(Pentry q[MAXPROCESSES]) {
     }
 
     for(proctmp=0; proctmp<MAXPROCESSES; proctmp++){
-        int *predictions;
+        PageStat *predictions;
         int i;
         if(!q[proctmp].active)
             continue;
         /* Guess the pages that the proc will need */
         predictions = pred_page(proctmp, q[proctmp].pc, cfg);
+        page_sort(predictions);
+        //print_guess(predictions);
         for(i=0; i<MAXPROCPAGES; i++){
-            if(predictions[i] == EMPTY)
+            if(predictions[i].page == EMPTY)
                 break;
-            pagein(proctmp, predictions[i]);
+            pagein(proctmp, predictions[i].page);
         }
     }
 
